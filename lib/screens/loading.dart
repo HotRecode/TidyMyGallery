a97b1e261b230de3a0e15274e0d30e70f2f0f9df
Fabian Oelscher <fabian.oelscher@tutanota.com>
2021-03-31 12:20:31 +0200

    Extract loading indicator for reuse

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,31 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:tidy_my_gallery/widgets/loading-indicator.dart';
 
 class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.orange[400],
-      child: Center(
-        child: Stack(children: [
-          Padding(
-            padding: EdgeInsets.fromLTRB(12, 5, 0, 0),
-            child: Text(
-              'Y³',
-              style: TextStyle(
-                fontFamily: 'Lobster',
-                color: Colors.white,
-                decoration: TextDecoration.none,
-                fontSize: 80,
-              ),
-            ),
-          ),
-          SpinKitCubeGrid(
-            color: Colors.orange[900],
-            size: 120.0,
-          ),
-        ]),
-      ),
-    );
+    return loadingIndicator();
   }
 }
---
+++ lib/widgets/loading-indicator.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+Widget loadingIndicator() {
+  return Container(
+    color: Colors.orange[400],
+    child: Center(
+      child: Stack(children: [
+        Padding(
+          padding: EdgeInsets.fromLTRB(12, 5, 0, 0),
+          child: Text(
+            'Y³',
+            style: TextStyle(
+              fontFamily: 'Lobster',
+              color: Colors.white,
+              decoration: TextDecoration.none,
+              fontSize: 80,
+            ),
+          ),
+        ),
+        SpinKitCubeGrid(
+          color: Colors.orange[900],
+          size: 120.0,
+        ),
+      ]),
+    ),
+  );
+}
